94b1df5744b66108bb14bd862f37f76654e7fae2
blue3 <blue3.do@gmail.com>
2021-02-18 22:44:30 +0700

    Create Lasthitrule Exporting

---
+++ Tufin-Python/Lasthit-Rules-Exporting.py
@@ -0,0 +1,6 @@
+# GET https://<Tufin_IP>/securetrack/api/devices/1/rules/351/documentation
+# => Comment = PUT
+# Body: XML
+# <rule_documentation>
+#   <comment>New Comment</comment>
+# </rule_documentation>
